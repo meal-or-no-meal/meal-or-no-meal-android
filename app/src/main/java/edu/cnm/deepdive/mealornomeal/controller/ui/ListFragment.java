5f358a38d7188ffde6e7e5ccdc85a246c9f61e3f
Ambar Rodriguez <ambarrodriguez80@yahoo.com>
2020-08-03 12:57:38 -0600

    Added icons.

--- app/src/main/java/edu/cnm/deepdive/mealornomeal/controller/ui/ListFragment.java
+++ app/src/main/java/edu/cnm/deepdive/mealornomeal/controller/ui/ListFragment.java
@@ -24,7 +24,7 @@ public class ListFragment extends Fragment implements OnIngredientClickListener 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
-      View view = inflater.inflate(R.layout.ingredient_list, container, false);
+      View view = inflater.inflate(R.layout.fragment_ingredient_list, container, false);
       ingredientArray = view.findViewById(R.id.recycler_view);
       return view;
     }
